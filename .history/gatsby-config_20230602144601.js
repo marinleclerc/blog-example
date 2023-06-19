45156cbae7b954f78c14ca5c7317c2581abb46fc
marinleclerc <mleclerc@makethegrade.fr>
2023-06-19 14:51:32 +0200

    Finished Gatsby Tutorial Part 3

--- .history/gatsby-config_20230602144601.js
+++ .history/gatsby-config_20230602144601.js
@@ -5,9 +5,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-  ],
-
+  plugins: [],
 }
---
+++ .history/src/pages/index_20230619141532.js
@@ -0,0 +1,21 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Link to="/about">About</Link>
+
+    </main>
+  )
+}
+
+// You'll learn about this in the next task, just copy it for now
+export const Head = () => <title>Home Page</title>
+
+// Step 3: Export your component
+export default IndexPage
